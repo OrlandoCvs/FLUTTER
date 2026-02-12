5cc5f67e02517a474a88688c0ec72107b91b0c8a
OrlandoCvs <orlandocs03@outlook.com>
2026-02-11 22:31:07 -0700

    archivo inicial para primera tarea

---
+++ flutter_app_notas/README.md
@@ -0,0 +1,3 @@
+# flutter_app_notas
+
+A new Flutter project.
---
+++ flutter_app_notas/android/app/src/main/kotlin/com/example/flutter_app_notas/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_app_notas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
